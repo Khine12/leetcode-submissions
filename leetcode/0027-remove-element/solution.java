3c50604c28bc652205cfd7ad2db9b5bae821f738
Khine Zar Hein <khinezarhein1@gmail.com>
2025-09-06 01:05:46 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42 MB (99.96%)

--- leetcode/0027-remove-element/solution.java
+++ leetcode/0027-remove-element/solution.java
@@ -1,9 +1,10 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
         
+        int n = nums.length;
         int i = 0;
         
-        for (int j=0; j<nums.length; j++) {
+        for (int j=0; j<n; j++) {
             
             if(nums[j]!=val) {
                 
