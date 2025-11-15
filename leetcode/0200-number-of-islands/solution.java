641f044c082327b24bacdfcb67725b206e877c79
Khine Zar Hein <khinezarhein1@gmail.com>
2025-11-15 05:40:10 +0000

    [LeetCode Sync] Runtime - 7 ms (14.64%), Memory - 51.5 MB (86.50%)

--- leetcode/0200-number-of-islands/solution.java
+++ leetcode/0200-number-of-islands/solution.java
@@ -1,43 +1,49 @@
 class Solution {
     public int numIslands(char[][] grid) {
         
-        int m = grid.length, n = grid[0].length;
+        int rows = grid.length;
+        int cols = grid[0].length;
         
-        int count = 0;
+        int count =0;
         
         int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
         
-        for(int r = 0; r<m ; r++){
+        for (int r=0; r < rows ; r++){
             
-            for(int c = 0; c < n; c++){
+            for(int c=0; c < cols; c++){
                 
                 if(grid[r][c]=='1'){
                     
                     count++;
                     
                     ArrayDeque<int[]> q = new ArrayDeque<>();
-        
+                    
                     q.offer(new int[]{r,c});
-                        
+                    
                     grid[r][c] = '0';
                     
                     while(!q.isEmpty()){
                         
-                        int[] curr = q.poll();
+                        int[] cells = q.poll();
                         
-                        int x = curr[0], y = curr[1];
+                        int x = cells[0];
                         
-                        for(int[] d: dirs){
+                        int y = cells[1];
+                        
+                        for(int[] d : dirs){
                             
-                            int nx = x + d[0], ny = y + d[1];
+                            int nx = x + d[0];
                             
-                            if(nx >= 0 && nx < m && ny >= 0 && ny < n && grid[nx][ny]=='1'){
+                            int ny = y + d[1];
+                            
+                            if(nx>=0 && nx<rows && ny>=0 && ny<cols && grid[nx][ny]=='1'){
                                 
                                 grid[nx][ny] = '0';
                                 
                                 q.offer(new int[]{nx,ny});
                             }
                         }
+                        
                     }
                 }
             }
