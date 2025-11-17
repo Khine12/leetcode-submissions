d39a4dedddeca1badeb9a38320768397a6f00031
Khine Zar Hein <khinezarhein1@gmail.com>
2025-11-17 07:29:01 +0000

    [LeetCode Sync] Runtime - 2 ms (56.19%), Memory - 46.4 MB (26.79%)

---
+++ leetcode/0871-keys-and-rooms/solution.java
@@ -0,0 +1,38 @@
+class Solution {
+    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        
+        int n = rooms.size();
+        
+        boolean[] visited = new boolean[n];
+        
+        visited[0] = true;
+        
+        Stack <Integer> stack = new Stack<>();
+        
+        stack.push(0);
+        
+        while(!stack.isEmpty()){
+            
+            int currRoom = stack.pop();
+            
+            for(int keys : rooms.get(currRoom)){
+                
+                if(!visited[keys]){
+                    
+                    visited[keys] = true;
+                    
+                    stack.push(keys);
+                }
+            }
+        }
+        
+        for (boolean v : visited){
+            
+            if(!v){
+                
+                return false;
+            }
+        }
+        return true;
+    }
+}
