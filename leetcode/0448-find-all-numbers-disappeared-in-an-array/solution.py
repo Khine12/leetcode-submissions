cfca4452d20f7f33b048b788ea39859585c163ed
Khine Zar Hein <khinezarhein1@gmail.com>
2026-05-29 08:30:02 +0000

    [LeetCode Sync] Runtime - 30 ms (48.89%), Memory - 30.3 MB (83.53%)

---
+++ leetcode/0448-find-all-numbers-disappeared-in-an-array/solution.py
@@ -0,0 +1,18 @@
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        
+        n = len(nums)
+        ans = []
+        s = set()
+
+        for i in range(n):
+
+            s.add(nums[i])
+
+        for i in range(1,n+1):
+
+            if i not in s:
+
+                ans.append(i)
+
+        return ans
