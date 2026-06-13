959a7254995045ec2c342fcefcd39133fbea41de
Khine Zar Hein <khinezarhein1@gmail.com>
2026-06-13 22:29:24 +0000

    [LeetCode Sync] Runtime - 418 ms (94.50%), Memory - 0.0B (100.00%)

---
+++ leetcode/1327-last-person-to-fit-in-the-bus/solution.sql
@@ -0,0 +1,7 @@
+-- Write your PostgreSQL query statement below
+SELECT person_name
+FROM (SELECT person_name, SUM(weight) OVER (ORDER BY turn) AS cumulative_weight
+      FROM Queue)
+WHERE cumulative_weight <= 1000
+ORDER BY cumulative_weight DESC
+LIMIT 1;
